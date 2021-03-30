c2cac7653308f3c7cfb3b4ef449a1f94e631166f
Cameron <chegney6@gmail.com>
2021-03-30 14:06:21 +1100

    All of Cams Code/Test Scenes

---
+++ Assets/Test Scenes/Ham/Scripts/uiScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiScript : MonoBehaviour
+{
+    
+    public float timeRemaining = 30;
+    public bool timerIsRunning = false;
+    public Text timeText;
+    public Text doodScore;
+    private int punches;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+
+        doodScore.text = "Objects Punched: " + punches.ToString();
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                Application.LoadLevel("testo2");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            punches++;
+
+            doodScore.text = "Objects Punched: " + punches.ToString();
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{00}", seconds);
+    }
+}
