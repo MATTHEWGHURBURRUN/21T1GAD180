cd297a64334fb8563704f7a08254ad8e9b570413
Cameron <chegney6@gmail.com>
2021-04-16 12:07:05 +1000

    Added A title Screen (Still needs work)
    Made code and a little UI stuff. Not complete as of yet.

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+            //Press the Return/Enter key in order to load the level "testo2" from the built levels in the build settings.
+          if (Input.GetKeyDown(KeyCode.Return))
+          {
+
+                Application.LoadLevel("Game Scene");
+
+          }
+        
+    }
+}
